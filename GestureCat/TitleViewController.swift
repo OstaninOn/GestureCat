f1998c090ea05e218f0fa3e66b433a9c87b1aede
ostanin <berezko88@mail.ru>
2022-10-03 15:09:19 +0300

    Initial commit

---
+++ GestureCat/TitleViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TitleViewController.swift
+//  GestureCat
+//
+//  Created by  aleksandr on 3.10.22.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController {
+    
+    @IBOutlet weak var StartTitle: UIView!
+        
+    
+        override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemGray6.cgColor,UIColor.green.cgColor]
+        gradient.frame = StartTitle.bounds
+        StartTitle.layer.insertSublayer(gradient, at: 0)
+        StartTitle.layer.cornerRadius = 50
+        StartTitle.layer.masksToBounds = true
+        StartTitle.layer.borderWidth = 1
+    
+        let gradientView = CAGradientLayer()
+        gradientView.colors = [UIColor.yellow.cgColor,UIColor.systemPink.cgColor]
+        gradientView.frame = view.bounds
+        view.layer.insertSublayer(gradientView, at: 0)
+        view.layer.masksToBounds = true
+        
+
+        
+    }
+
+}
---
+++ GestureCat/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  GestureCat
+//
+//  Created by  aleksandr on 3.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var catView: UIView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragTheView))
+        catView.addGestureRecognizer(panGestureRecognizer)
+    }
+    @objc func dragTheView(recognizer: UIPanGestureRecognizer) {
+        
+        if recognizer.state == .began {
+        } else if recognizer.state == .changed {
+            
+            let translation = recognizer.translation(in: self.view)
+            tiltTheView(with: translation)
+            
+            let newX = catView.center.x + translation.x
+            let newY = catView.center.y + translation.y
+            
+            catView.center = CGPoint(x: newX, y: newY)
+            recognizer.setTranslation(CGPoint.zero, in: self.view)
+            
+        } else if recognizer.state == .ended {
+          
+            if catView.center.x < 20 {
+                UIView.animate(withDuration: 0.6, animations: { [unowned self] in
+                    self.catView.center.x = -self.catView.frame.size.width
+                })
+            } else if catView.center.x > self.view.frame.size.width - 20 {
+                UIView.animate(withDuration: 0.6, animations: { [unowned self] in
+                    self.catView.center.x = self.view.frame.size.width + self.catView.frame.size.width
+                })
+            }
+            
+            UIView.animate(withDuration: 0, delay: 0.6, animations: {
+                self.catView.transform = .identity
+                self.catView.center = self.view.center
+            })
+            
+        }
+      
+    }
+    private func tiltTheView(with translationValue: CGPoint) {
+        let translationMoved = self.view.center.x - self.catView.center.x
+        let divKoef = (self.view.frame.size.width / 1) / 1
+        
+        catView.transform = CGAffineTransform(rotationAngle: translationMoved / divKoef)
+    }
+}
+
