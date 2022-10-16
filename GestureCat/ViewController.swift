42ac070a446bf49bdbed793f011cf8c727f47a97
ostanin <berezko88@mail.ru>
2022-10-16 23:41:41 +0300

    add intersects

--- GestureCat/ViewController.swift
+++ GestureCat/ViewController.swift
@@ -3,28 +3,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
 
-    private let image = UIImage(named: "fish")
-    private lazy var imageView = UIImageView(image: image)
-    private lazy var imageViewSecond = UIImageView(image: image)
-    private lazy var imageViewThird = UIImageView(image: image)
+    let image = UIImage(named: "fish")
+    lazy var imageView = UIImageView(image: image)
+    lazy var imageViewSecond = UIImageView(image: image)
+    lazy var imageViewThird = UIImageView(image: image)
     
-    private let Ice = UIImage(named: "ice")
-    private lazy var imageViewIce = UIImageView(image: Ice)
-    private lazy var imageViewIceThird = UIImageView(image: Ice)
+    let Ice = UIImage(named: "ice")
+    lazy var imageViewIce = UIImageView(image: Ice)
+    lazy var imageViewIceThird = UIImageView(image: Ice)
     
-    private let imageSausage = UIImage(named: "sausage")
-    private lazy var imageSausageFirst = UIImageView(image: imageSausage)
-    private lazy var imagSausageSecond = UIImageView(image: imageSausage)
+    let imageSausage = UIImage(named: "sausage")
+    lazy var imageSausageFirst = UIImageView(image: imageSausage)
+    lazy var imagSausageSecond = UIImageView(image: imageSausage)
     
-    private var isGaming = true
+    var isGaming = true
+    var Gaming = true
+    var GamingSausage = true
     
     @IBOutlet weak var catView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+      
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 30, y: -100, width: 50, height: 50)
         view.addSubview(imageViewSecond)
@@ -35,7 +36,7 @@ class ViewController: UIViewController {
         view.addSubview(imageViewIce)
         imageViewIce.frame = CGRect(x: 30, y: -150, width: 100, height: 100)
         view.addSubview(imageViewIceThird)
-        imageViewIceThird.frame = CGRect(x: 250, y: -80, width: 100, height: 100)
+        imageViewIceThird.frame = CGRect(x: 270, y: -80, width: 100, height: 100)
         
         view.addSubview(imageSausageFirst)
         imageSausageFirst.frame = CGRect(x: 80, y: -150, width: 60, height: 60)
@@ -49,9 +50,7 @@ class ViewController: UIViewController {
     }
     
     
-    
     @objc func dragTheView(recognizer: UIPanGestureRecognizer) {
-        
         
         if recognizer.state == .began {
         } else if recognizer.state == .changed {
@@ -64,8 +63,8 @@ class ViewController: UIViewController {
             
             catView.center = CGPoint(x: newX, y: newY)
             recognizer.setTranslation(CGPoint.zero, in: self.view)
-            } else if recognizer.state == .ended {
-          
+        } else if recognizer.state == .ended {
+            
             if catView.center.x < 20 {
                 UIView.animate(withDuration: 0.6, animations: { [unowned self] in
                     self.catView.center.x = -self.catView.frame.size.width
@@ -82,21 +81,21 @@ class ViewController: UIViewController {
             })
             
         }
-      
+        
     }
     
     private func tiltTheView(with translationValue: CGPoint) {
         _ = self.view.center.x - self.catView.center.x
-      
+        
     }
-  
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         UIView.animate(withDuration: 4, delay: 4, options: [
             .curveLinear, .repeat], animations: {
                 self.imageView.frame.origin.y +=
-                self.view.frame.width + 1000
+                self.view.frame.width + 900
             })
         
         UIView.animate(withDuration: 3, delay: 5, options: [
@@ -111,13 +110,13 @@ class ViewController: UIViewController {
                 self.view.frame.width + 900
             })
         
-        UIView.animate(withDuration: 4, delay: 10, options: [
+        UIView.animate(withDuration: 6, delay: 10, options: [
             .curveLinear, .repeat], animations: {
                 self.imageViewIce.frame.origin.y +=
                 self.view.frame.width + 400
             })
         
-        UIView.animate(withDuration: 6, delay: 3, options: [
+        UIView.animate(withDuration: 8, delay: 3, options: [
             .curveLinear, .repeat], animations: {
                 self.imageViewIceThird.frame.origin.y +=
                 self.view.frame.width + 400
@@ -136,50 +135,147 @@ class ViewController: UIViewController {
             })
         
         intersects()
+        intersectsFirstFish()
+        intersectsSecondFish()
+        intersectsThirdFish()
+        intersectsSausageFirst()
+        intersectsSausageSecond()
         
     }
 
-    
-    
-    
-    private func intersects() {
-            guard isGaming else { return }
-            if checkIntersect(catView, imageViewIce)
-                || checkIntersect(catView, imageViewIceThird) {
-                //|| checkIntersect(catView, imageViewIceThird) {
-                print("GAME OVER")
-                imageViewIce.layer.removeAllAnimations()
-                imageViewIceThird.layer.removeAllAnimations()
-                //imageViewIceThird.layer.removeAllAnimations()
-                print("GAME STOPPED")
-                isGaming = false
-                
-                let alert = UIAlertController(title: "GAME OVER", message: "попробуй еще раз", preferredStyle: .alert)
-
-                let ViewBack = storyboard?.instantiateViewController(withIdentifier: "transition")
-                let okButton = UIAlertAction(title: "OK", style: .default, handler: {_ in self.navigationController?.pushViewController(ViewBack!, animated: true)
-                })
-                
-                alert.addAction(okButton)
-                self.present(alert, animated: true, completion: nil)
-                
-            }
+    func intersects() {
+        guard isGaming else { return }
+        if checkIntersect(catView, imageViewIce)
+            || checkIntersect(catView, imageViewIceThird) {
+            print("GAME OVER")
+            imageViewIce.layer.removeAllAnimations()
+            imageViewIceThird.layer.removeAllAnimations()
+            print("GAME STOPPED")
+            isGaming = false
             
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+            let alert = UIAlertController(title: "GAME OVER", message: "попробуй еще раз 🙊", preferredStyle: .alert)
+            let ViewBack = storyboard?.instantiateViewController(withIdentifier: "transition")
+            let okButton = UIAlertAction(title: "OK", style: .default, handler: {_ in self.navigationController?.pushViewController(ViewBack!, animated: true)
+            })
+            
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
             self.intersects()
+        }
+     
+    }
+    
+    func checkIntersect(_ first: UIView, _ second: UIView) -> Bool {
+        guard let firstFrame = first.layer.presentation()?.frame,
+              let secondFrame = second.layer.presentation()?.frame else { return true }
+        
+        return firstFrame.intersects(secondFrame)
+        }
+    
+    
+    func intersectsFirstFish() {
+        guard Gaming else { return }
+        if checkIntersect(catView, imageView){
+            print("fish")
+            imageView.layer.removeAllAnimations()
+            Gaming = true
+            imageView.frame = CGRect(x: 30, y: -100, width: 50, height: 50)
+            UIView.animate(withDuration: 6, delay: 4, options: [
+                .curveLinear, .repeat], animations: {
+                    self.imageView.frame.origin.y +=
+                    self.view.frame.width + 800
+                })
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.intersectsFirstFish()
+        }
+        
+    }
+   
+    func intersectsSecondFish() {
+        guard Gaming else { return }
+        if checkIntersect(catView, imageViewSecond){
+            print("fish")
+            imageViewSecond.layer.removeAllAnimations()
+            Gaming = true
+            imageViewSecond.frame = CGRect(x: 150, y: -150, width: 50, height: 50)
+            UIView.animate(withDuration: 3, delay: 5, options: [
+                .curveLinear, .repeat], animations: {
+                    self.imageViewSecond.frame.origin.y +=
+                    self.view.frame.width + 600
+                })
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.intersectsSecondFish()
         }
         
     }
     
-    private func checkIntersect(_ first: UIView, _ second: UIView) -> Bool {
+    func intersectsThirdFish() {
+        guard Gaming else { return }
+        if checkIntersect(catView, imageViewThird){
+            print("fish")
+            imageViewThird.layer.removeAllAnimations()
+            Gaming = true
+            imageViewThird.frame = CGRect(x: 300, y: -150, width: 50, height: 50)
+            UIView.animate(withDuration: 8, delay: 2, options: [
+                .curveLinear, .repeat], animations: {
+                    self.imageViewThird.frame.origin.y +=
+                    self.view.frame.width + 900
+                })
+        }
         
-        guard let firstFrame = first.layer.presentation()?.frame,
-              let secondFrame = second.layer.presentation()?.frame else { return false }
-        
-        return firstFrame.intersects(secondFrame)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.intersectsThirdFish()
+        }
         
     }
     
+    func intersectsSausageFirst() {
+        guard Gaming else { return }
+        if checkIntersect(catView, imageSausageFirst){
+            print("fish")
+            imageSausageFirst.layer.removeAllAnimations()
+            Gaming = true
+            imageSausageFirst.frame = CGRect(x: 80, y: -150, width: 60, height: 60)
+            UIView.animate(withDuration: 5, delay: 3, options: [
+                .curveLinear, .repeat], animations: {
+                    self.imageSausageFirst.frame.origin.y +=
+                    self.view.frame.width + 800
+                })
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.intersectsSausageFirst()
+        }
+        
+    }
+    
+    func intersectsSausageSecond() {
+        guard Gaming else { return }
+        if checkIntersect(catView, imagSausageSecond){
+            print("fish")
+            imagSausageSecond.layer.removeAllAnimations()
+            GamingSausage = true
+            imagSausageSecond.frame = CGRect(x: 300, y: -60, width: 60, height: 60)
+            UIView.animate(withDuration: 5, delay: 8, options: [
+                .curveLinear, .repeat], animations: {
+                    self.imagSausageSecond.frame.origin.y +=
+                    self.view.frame.width + 400
+                })
+        }
 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.intersectsSausageSecond()
+        }
+
+    }
+
+    
 }
 
